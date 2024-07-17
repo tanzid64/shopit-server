5b10572a22b613c6a3537e174e57b3f4d65a4324
tanzid64 <tanzid@inbox.ru>
2024-07-18 03:39:35 +0600

    create user done

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.js";
 import { newUserRequestBody } from "../types/types.js";
 import { TryCatch } from "../middlewares/error.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 export const newUser = TryCatch(
   async (
@@ -10,6 +11,16 @@ export const newUser = TryCatch(
     next: NextFunction
   ) => {
     const { name, email, photo, gender, dob, _id } = req.body;
+    let user = await User.findById(_id);
+    if (user) {
+      return res.status(200).json({
+        success: true,
+        message: `Welcome, ${user.name}!`,
+      });
+    }
+    if (!name || !email || !photo || !gender || !dob || !_id) {
+      next(new ErrorHandler("Please provide all fields", 400));
+    }
     await User.create({ name, email, photo, gender, dob: new Date(dob), _id });
     return res.status(201).json({
       success: true,
