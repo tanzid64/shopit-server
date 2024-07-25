1aace8104ae8f2843b2eef52a457c43b2fab9f59
tanzid64 <tanzid@inbox.ru>
2024-07-26 01:49:54 +0600

    validation added..

--- dist/controllers/order.js
+++ dist/controllers/order.js
@@ -2,8 +2,21 @@ import { TryCatch } from "../middlewares/error.js";
 import { Order } from "../models/order.js";
 import { reduceStock } from "../utils/orderHelper.js";
 import { invalidatesCache } from "../utils/revalidateCache.js";
+import ErrorHandler from "../utils/errorHandler.js";
 export const newOrder = TryCatch(async (req, res, next) => {
     const { orderItems, shippingInfo, user, subTotal, tax, shippingCharges, discount, total, } = req.body;
+    if (!orderItems)
+        return next(new ErrorHandler("Please add atleast one item", 400));
+    if (!shippingInfo ||
+        !user ||
+        !subTotal ||
+        !tax ||
+        !shippingCharges ||
+        !discount ||
+        !total) {
+        return next(new ErrorHandler("Please provide all fields", 400));
+    }
+    // create order
     await Order.create({
         orderItems,
         shippingInfo,
@@ -14,6 +27,7 @@ export const newOrder = TryCatch(async (req, res, next) => {
         discount,
         total,
     });
+    // reduce stock
     reduceStock(orderItems);
     invalidatesCache({ product: true, admin: true });
     return res.status(201).json({
--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -4,6 +4,7 @@ import { newOrderReqBody } from "../types/types.js";
 import { Order } from "../models/order.js";
 import { reduceStock } from "../utils/orderHelper.js";
 import { invalidatesCache } from "../utils/revalidateCache.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 export const newOrder = TryCatch(
   async (req: Request<{}, {}, newOrderReqBody>, res, next) => {
@@ -17,6 +18,21 @@ export const newOrder = TryCatch(
       discount,
       total,
     } = req.body;
+    if (!orderItems)
+      return next(new ErrorHandler("Please add atleast one item", 400));
+    if (
+      !shippingInfo ||
+      !user ||
+      !subTotal ||
+      !tax ||
+      !shippingCharges ||
+      !discount ||
+      !total
+    ) {
+      return next(new ErrorHandler("Please provide all fields", 400));
+    }
+
+    // create order
     await Order.create({
       orderItems,
       shippingInfo,
@@ -27,6 +43,8 @@ export const newOrder = TryCatch(
       discount,
       total,
     });
+
+    // reduce stock
     reduceStock(orderItems);
     invalidatesCache({ product: true, admin: true });
 
